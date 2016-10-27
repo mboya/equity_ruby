8e56b4989303e366bdfffb4064475c14f794d245
mboya <mboyaberry@gmail.com>
2016-10-27 18:00:47 +0300

    assists for the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'equity_ruby'
-
+require 'webmock/minitest'
 require 'minitest/autorun'
+require 'pry'
+
+WebMock.disable_net_connect!
+
